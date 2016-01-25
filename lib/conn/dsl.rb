f0cf8b726cd5b0c70bd4d7c409a17dc77ef54be7
Uchio KONDO <udzura@udzura.jp>
2016-01-25 18:37:56 +0900

    Split to_ssh_config

--- lib/conn/dsl.rb
+++ lib/conn/dsl.rb
@@ -8,14 +8,12 @@ require 'conn/pty_ssh'
 module Conn
   module DSL
     def ssh(hostname, &blk)
-      config = Net::SSH::Config.for(hostname)
-      user = config[:user] || Etc.getlogin
       queue = Queue.new
       cmd_loop = Thread.new do
         blk.call(queue)
         queue.respond_to?(:close) ? queue.close : (queue << false)
       end
-      Net::SSH.start(hostname, user, config) do |ssh|
+      Net::SSH.start(*to_ssh_config(hostname)) do |ssh|
         ssh.loop do
           msg = queue.pop
           if msg
@@ -36,6 +34,16 @@ module Conn
       cmd_loop.join if cmd_loop.alive?
     end
 
+    def ssh_try!(hostname)
+    end
+
+    private
+    def to_ssh_config(hostname)
+      config = Net::SSH::Config.for(hostname)
+      user = config[:user] || Etc.getlogin
+      [hostname, user, config]
+    end
+
     def input(str)
       puts "%s %s" % ["SSH<<".green.bold, str.yellow]
     end
@@ -48,16 +56,15 @@ module Conn
       puts "%s %s" % ["SSH!>".red.bold, str.red]
     end
 
+    public
     using Conn::PtySSH
     def ssh_pty(hostname, &blk)
-      config = Net::SSH::Config.for(hostname)
-      user = config[:user] || Etc.getlogin
       queue = Queue.new
       cmd_loop = Thread.new do
         blk.call(queue)
         queue.respond_to?(:close) ? queue.close : (queue << false)
       end
-      Net::SSH.start(hostname, user, config) do |ssh|
+      Net::SSH.start(*to_ssh_config(hostname)) do |ssh|
         ssh.loop do
           msg = queue.pop
           if msg
